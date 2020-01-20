c126f3d42681d38364884c878f66c7ddc59698ab
serii <seriiburduja@gmail.com>
2020-01-20 14:27:41 +0200

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,54 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::group(['middleware', 'web'], function () {
+	Route::match(['get', 'post'], '/', ['uses' => 'IndexController@execute', 'as' => 'home']);
+});
+
+Route::get('/page/{alias}', ['uses' => 'PageController@execute', 'as' => 'page']);
+
+Route::auth();
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+	//admin
+	Route::get('/', function () {
+
+	});
+
+	//admin/pages
+	Route::group(['prefix' => 'pages'], function () {
+		//admin/pages
+		Route::get('/', ['uses' => 'PagesController@execute', 'as' => 'pages']);
+
+		//admin/add
+		Route::match(['get', 'post'], '/add', ['uses' => 'PagesAddController@execute', 'as' => 'pagesAdd']);
+		//admin/edit/id
+		Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses' => 'PagesEditController@execute', 'as' => 'pagesEdit']);
+	});
+
+	//admin/portfolio
+	Route::group(['prefix' => 'portfolio'], function () {
+		//admin/portfolio
+		Route::get('/', ['uses' => 'PortfolioController@execute', 'as' => 'portfolio']);
+
+		//admin/add
+		Route::match(['get', 'post'], '/add', ['uses' => 'PortfolioAddController@execute', 'as' => 'portfolioAdd']);
+		//admin/edit/id
+		Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses' => 'PortfolioEditController@execute', 'as' => 'portfolioEdit']);
+	});
+
+	//admin/services
+	Route::group(['prefix' => 'services'], function () {
+		//admin/services
+		Route::get('/', ['uses' => 'ServiceController@execute', 'as' => 'services']);
+
+		//admin/add
+		Route::match(['get', 'post'], '/add', ['uses' => 'ServiceAddController@execute', 'as' => 'servicesAdd']);
+		//admin/edit/id
+		Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses' => 'ServiceEditController@execute', 'as' => 'servicesEdit']);
+	});
 });
