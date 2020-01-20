9aa49c8c3ff319394a85ece2b883e6311dfb4206
serii <seriiburduja@gmail.com>
2020-01-20 13:32:46 +0200

    add data in databases

---
+++ database/migrations/2020_01_20_083231_create_table_pages.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+	        $table->string('alias', 100);
+	        $table->string('image', 200);
+	        $table->text('text');
+	        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pages');
+    }
+}
