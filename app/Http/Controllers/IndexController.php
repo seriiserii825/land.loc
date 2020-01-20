b28280f2206678a8ada31b1dd1dd634f81b229a1
serii <seriiburduja@gmail.com>
2020-01-20 15:35:05 +0200

    разделил шаблон на отдельные элементы

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
 
 class IndexController extends Controller
 {
 	public function execute(Request $request){
-		return view('layouts.site');
+		$pages = Page::all();
+		$portoflios = Portfolio::get(['name', 'image', 'filter']);
+		$services = Service::all();
+		$peoples = People::take(3)->get();
+		return view('front.index');
 	}
 }
---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.site')
+
+@section('header')
+    @include('front.header')
+@endsection
+
+@section('content')
+    @include('front.content')
+@endsection
